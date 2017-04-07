340ab9a68600cf685b22ba1e18190e756f4f9d66
Archimedes Trajano <arch@trajano.net>
2017-04-07 16:30:07 -0400

    Compiles successfully to EAR but does not work with WAS8.5 security yet

--- entrust-truepass-jaspic/src/main/java/net/trajano/entrust/jaspic/EntrustTruePassServletContextInitializer.java
+++ entrust-truepass-jaspic/src/main/java/net/trajano/entrust/jaspic/EntrustTruePassServletContextInitializer.java
@@ -1,9 +1,12 @@
 package net.trajano.entrust.jaspic;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import javax.security.auth.message.config.AuthConfigFactory;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
@@ -16,9 +19,16 @@ public class EntrustTruePassServletContextInitializer implements
     @Override
     public void contextInitialized(final ServletContextEvent sce) {
 
-        final Map<String, String> options = new ConcurrentHashMap<String, String>();
-        AuthConfigFactory.getFactory()
-            .registerConfigProvider(EntrustTruePassAuthModuleConfigProvider.class.getName(), options, "HttpServlet", null, null);
+        try {
+            final Map<String, String> options = new ConcurrentHashMap<String, String>();
+            options.put("com.ibm.websphere.jaspi.configuration",
+                (new File((File) sce.getServletContext().getAttribute(ServletContext.TEMPDIR),
+                    "jaspi-registrations")).getCanonicalPath());
+            AuthConfigFactory.getFactory().registerConfigProvider(
+                EntrustTruePassAuthModuleConfigProvider.class.getName(), options, "HttpServlet", null, null);
+        } catch (IOException e) {
+            throw new IllegalStateException("IOException during initialization", e);
+        }
     }
 
     /**
